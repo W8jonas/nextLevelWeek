e69021bfb13eb26b4fbc412a792e9f5d16986ff6
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-07 17:25:26 -0300

    criando seeds

---
+++ Ecoleta/server/src/database/seeds/create_items.ts
@@ -0,0 +1,11 @@
+import Knex from 'knex'
+
+export async function seed(kenx: Knex) {
+    await Knex('items').insert([
+        {title: 'Lampadas', image: 'lampdas.svg'},
+        {title: 'Pilhas e baterias', image: 'papeis-papelao.svg'},
+        {title: 'Papéis e papelão', image: 'eletronicos.svg'},
+        {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
+        {title: 'Ôleo de cozinha', image: 'oleo.svg'},
+    ])
+}
