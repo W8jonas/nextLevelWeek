bf850fde7083984cd2576ccb4ff884452346fa1a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-09 22:09:09 -0300

    integraçaõ com IBGE

--- Ecoleta/web/src/pages/createPoint/index.tsx
+++ Ecoleta/web/src/pages/createPoint/index.tsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, {useState, useEffect, ChangeEvent} from 'react'
 import { Link } from "react-router-dom"
 import { FiArrowLeft } from "react-icons/fi";
 import './styles.css'
 import { Map, TileLayer, Marker } from 'react-leaflet'
+import api from '../../services/apis'
+import axios from 'axios'
 
 import logo from "../../assets/logo.svg"
 
+interface Item {
+    id: Number,
+    title: string,
+    image_url: string
+}
+
+interface IBGEResponse {
+    sigla: string
+}
+
+interface IBGECityResponse {
+    nome: string
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
+    const [cities, setCities] = useState<string[]>([])
+
+    const [selectedUf, setSelectedUf] = useState('0')
+    const [selectedCity, setSelectedCity] = useState('0')
+
+    useEffect(()=>{
+       api.get('items').then((response)=>{
+            setItems(response.data)
+       }) 
+    },[])
+
+    useEffect(()=>{
+        axios.get<IBGEResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then((response)=>{
+            const ufInitials = response.data.map((uf)=>uf.sigla)
+            setUfs(ufInitials)
+       }) 
+    },[])
+
+    useEffect(()=>{
+        if(selectedUf !== '0'){
+            axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+            .then((response)=>{
+                const cityNames = response.data.map((city)=>city.nome)
+                setCities(cityNames)
+           }) 
+        }
+    },[selectedUf])
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedUf(event.target.value)
+    }
+
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedCity(event.target.value)
+    }
     return (
         <div id="page-create-point">
             <header>
@@ -65,14 +120,36 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select 
+                                name="uf" 
+                                id="uf" 
+                                value={selectedUf} 
+                                onChange={handleSelectUf}
+                            >
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map((uf)=>(
+                                    <option 
+                                        key={`${uf}`}
+                                        value={uf}>{uf}
+                                    </option>
+                                ))}
                             </select>
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select 
+                                name="city" 
+                                id="city" 
+                                value={selectedCity} 
+                                onChange={handleSelectCity}
+                            >
                                 <option value="0">Selecione uma cidade</option>
+                                {cities.map((city)=>(
+                                    <option 
+                                        key={`${city}`}
+                                        value={city}>{city}
+                                    </option>
+                                ))}
                             </select>
                         </div>
                     </div>
@@ -85,24 +162,16 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                        </li>
+
+                        {items.map((item)=>(
+                            <li key={`${item.id}`}>
+                                <img 
+                                    src={item.image_url} 
+                                    alt={`${item.title}`}
+                                />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
 
