500154de5c6830744c1045229c35ffb7ead89a73
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-09 21:29:09 -0300

    adicionado item pilhas e baterias ao banco de dados

--- Ecoleta/server/src/database/seeds/create_items.ts
+++ Ecoleta/server/src/database/seeds/create_items.ts
@@ -2,10 +2,11 @@ import Knex from 'knex'
 
 export async function seed(knex: Knex) {
     await knex('items').insert([
-        {title: 'Lampadas', image: 'lampdas.svg'},
-        {title: 'Pilhas e baterias', image: 'papeis-papelao.svg'},
-        {title: 'Papéis e papelão', image: 'eletronicos.svg'},
+        {title: 'Lampadas', image: 'lampadas.svg'},
+        {title: 'Papéis e papelão', image: 'papeis-papelao.svg'},
+        {title: 'Eletrônicos', image: 'eletronicos.svg'},
         {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
         {title: 'Ôleo de cozinha', image: 'oleo.svg'},
+        {title: 'Pilhas e baterias', image: 'baterias.svg'},
     ])
 }
